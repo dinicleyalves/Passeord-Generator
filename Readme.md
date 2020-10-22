79899e7a665fd592b158ac4d578842e7e5bbf2a0
Gustavo <gustavoolrosa2019@gmail.com>
2020-10-22 20:09:19 -0300

    Mini Software OK

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Gerador de Senhas
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](#)
+
+
+![](image.PNG)
+
+Neste repositório, trago o código fonte de um gerador de senhas. O objetivo deste repositório é ajudar a comunidade a se desenvolver , além de manter afiada minhas habilidades desenvolvendo usando a linguagem PHP.
+
+# Ferramentas presentes na versão atual:
+
+  - Gerando senhas com tamanho dinâmico.
+  - Gerando senhas com letras maiusculas.
+  - Gerando senhas com numeros.
+  - Gerando senhas com simbolos.
+  
+# Ideias para possiveis atualizações:
+
+- Nenhuma ideia.
+
+
+### Como instalar
+
+Para usar estes arquivos, crie uma conta em um site de hospedagem como o 000webhost ou baixe um servidor em seu computador, como por exemplo o WampServer com suporte para PHP e arraste os arquivos para dentro e se divirta.
+
+### Observação
+
+Como o objetivo deste repositório é treinar minhas habilidades com php, não codifiquei o front-end do zero.
+Link do código inicial:
+
+https://bootsnipp.com/snippets/Q0k96
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+include_once("./passgen.php");
+
+$senha = 'Undefined';
+
+if (isset($_POST['qtdUsuarioDefined'])){
+$gerando = generate($_POST['qtdUsuarioDefined']);
+$senha = $gerando;
+}
+?>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <section class="Material-contact-section section-padding section-dark mt-4">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12 wow animated fadeInLeft mt-4" data-wow-delay=".2s">
+                  <h1 class="section-title mt-4">Zenitsu Pass Generator</h1>
+              </div>
+          </div>
+          <div class="row mt-4">
+              <div class="col-md-6 mt-3 contact-widget-section2 wow animated fadeInLeft" data-wow-delay=".2s">
+                <p>Para gerar a senha é bem simples, insira uma quantidade de caracteres e clique em gerar senha!</p>
+
+                <div class="find-widget">
+                 Repositório GitHub do projeto:  <a href="https://github.com/GustavoOliveiraRosa/passwordgenerator">https://github.com/GustavoOliveiraRosa/passwordgenerator</a>
+                </div>
+
+                <div class="find-widget">
+                 Perfil GitHub:  <a href="https://github.com/GustavoOliveiraRosa">https://github.com/GustavoOliveiraRosa</a>
+                </div>
+    
+    
+              </div>
+              <div class="col-md-6 wow animated fadeInRight" data-wow-delay=".2s">
+                  <form class="shake" role="form" method="post" id="contactForm" name="contact-form" data-toggle="validator">
+                      
+                  <div class="form-group label-floating mt-4">
+                        <label class="control-label" for="name">Quantidade de Caracteres</label>
+                        <input class="form-control" id="name" type="number" name="qtdUsuarioDefined" data-error="Please enter your name">
+                        <div class="help-block with-errors"></div>
+                      </div>
+                      
+                      <div class="form-group label-floating mt-4">
+                        <input class="form-control" id="name" type="text" value="<?php echo $senha ?>" required data-error="Please enter your name" disabled>
+                        <div class="help-block with-errors"></div>
+                      </div>
+                      
+                  <div class="form-submit mt-5">
+                          <button class="btn btn-common" type="submit" id="form-submit"><i class="material-icons mdi mdi-message-outline"></i>Gerar Senha</button>
+                          <div id="msgSubmit" class="h3 text-center hidden"></div>
+                          <div class="clearfix"></div>
+                      </div>
+
+                  
+
+                  </form>
+              </div>
+          </div>
+      </div>
+    </section>
